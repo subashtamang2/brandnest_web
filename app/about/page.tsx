dab3b336b7b261672d3460d5039edb554d624806
SubashLama123 <lamasubaah2@gmail.com>
2025-07-19 21:04:36 +0545

    ch

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,7 +3,7 @@
 const Aboutpage = () => {
   return (
     <div className="mt-50">
-      <div className="bg-blue-950">
+      <div className="bg-blue-950 text-white  w-full">About Us
 
       </div>
 
